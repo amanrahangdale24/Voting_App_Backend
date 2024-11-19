19e329bb49a3a354262c4406d5e4c53e8b6daa39
Aman Rahangdale <147967516+amanrahangdale24@users.noreply.github.com>
2024-11-19 12:20:45 +0530

    connected to mongo public url

--- db/config/connection.js
+++ db/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 function connectToDB(){
-    mongoose.connect(process.env.MONGO_URL).then(()=>{
+    mongoose.connect(process.env.MONGO_PUBLIC_URL).then(()=>{
         console.log("connected to DB")
     })
 }
